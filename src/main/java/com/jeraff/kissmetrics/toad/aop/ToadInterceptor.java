18f5cc4167b9c9abd8cb3d13ec9e04d5c7d82d33
Arin Sarkissian <arin@rspot.net>
2012-08-24 14:03:06 -0700

    remove serr on 'ToadInterceptor:invoke - successful run'

--- src/main/java/com/jeraff/kissmetrics/toad/aop/ToadInterceptor.java
+++ src/main/java/com/jeraff/kissmetrics/toad/aop/ToadInterceptor.java
@@ -1,12 +1,11 @@
 package com.jeraff.kissmetrics.toad.aop;
 
 import com.jeraff.kissmetrics.client.KissMetricsException;
-import com.jeraff.kissmetrics.toad.ToadELHelper;
 import com.jeraff.kissmetrics.toad.Toad;
+import com.jeraff.kissmetrics.toad.ToadELHelper;
 import com.jeraff.kissmetrics.toad.ToadProvider;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.omg.PortableServer._ServantActivatorStub;
 
 public class ToadInterceptor implements MethodInterceptor {
     private ToadProvider toadProvider;
@@ -38,7 +37,6 @@ public class ToadInterceptor implements MethodInterceptor {
                             result = invocation.proceed();
                             elHelper.populateToad(kissAnnotation);
                             toad.run();
-                            System.err.println("ToadInterceptor:invoke - successful run");
                             return result;
                         }
                     }
